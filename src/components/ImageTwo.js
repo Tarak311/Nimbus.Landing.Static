10b53ddc3ae493102a6ad4fbb3bb6b47bdd9fd96
Tarak Patel <tarak_1@live.com>
2023-02-20 03:34:22 -0500

    minor change

--- src/components/ImageTwo.js
+++ src/components/ImageTwo.js
@@ -4,7 +4,7 @@ import Blue from '../img/blue.jpg'
 const ImageTwo = () => (
     <Parallax className='image' blur={0} bgImage={Blue} strength={600} bgImageStyle={{minHeight:"100vh"}}>
         <div className='content' >
-            <span className='img-txt'>a trip to space</span>
+            <span className='img-txt'>Landing Page , please select your options </span>
         </div>
     </Parallax>
 );
